50748aca54c16b28ede2ad53a769188bedaf1063
daria <dariae9@yahoo.com>
2023-12-29 13:51:22 +0200

    reused code

--- menu.py
+++ menu.py
@@ -58,32 +58,8 @@ def display_screen(message):
     text = text.render(message, True, (255, 255, 255))
     text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 
-    draw_game_buttons()
-
     win.blit(text, text_rect)
-    pygame.display.update()
-
-    run = True
-    clock = pygame.time.Clock()
-
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if (
-                        RETURN_BUTTON["x"] < mouse_pos[0] < RETURN_BUTTON["x"] + RETURN_BUTTON["width"]
-                        and RETURN_BUTTON["y"] < mouse_pos[1] < RETURN_BUTTON["y"] + RETURN_BUTTON["height"]
-                ):
-                    menu()
-                elif (
-                        EXIT_BUTTON["x"] < mouse_pos[0] < EXIT_BUTTON["x"] + EXIT_BUTTON["width"]
-                        and EXIT_BUTTON["y"] < mouse_pos[1] < EXIT_BUTTON["y"] + EXIT_BUTTON["height"]
-                ):
-                    sys.exit()
-
-        clock.tick(60)
+    draw_game_buttons()
 
 
 def display_rules_screen():
@@ -114,6 +90,26 @@ def display_rules_screen():
 
     draw_game_buttons()
 
+
+def init_game(game_title, hex_size, map_rows, map_cols, colored_percentage):
+    pygame.display.set_caption(game_title)
+    clock = pygame.time.Clock()
+    run = True
+    win.fill(PINK)
+
+    # creating the map
+    hexagons, start_x, total_width, start_y, total_height = generate_hexagon_map(map_rows, map_cols, hex_size, WIDTH,
+                                                                                 HEIGHT,
+                                                                                 colored_percentage)
+
+    return hexagons, start_x, total_width, start_y, total_height, clock, run
+
+
+def draw_game_buttons():
+    draw_button(RETURN_BUTTON["x"], RETURN_BUTTON["y"], RETURN_BUTTON["width"], RETURN_BUTTON["height"],
+                "Return to Main Menu", lambda: menu())
+    draw_button(EXIT_BUTTON["x"], EXIT_BUTTON["y"], EXIT_BUTTON["width"], EXIT_BUTTON["height"], "Exit",
+                lambda: sys.exit())
     pygame.display.update()
 
     run = True
@@ -137,27 +133,6 @@ def display_rules_screen():
                     sys.exit()
 
         clock.tick(60)
-
-
-def init_game(game_title, hex_size, map_rows, map_cols, colored_percentage):
-    pygame.display.set_caption(game_title)
-    clock = pygame.time.Clock()
-    run = True
-    win.fill(PINK)
-
-    # creating the map
-    hexagons, start_x, total_width, start_y, total_height = generate_hexagon_map(map_rows, map_cols, hex_size, WIDTH,
-                                                                                 HEIGHT,
-                                                                                 colored_percentage)
-
-    return hexagons, start_x, total_width, start_y, total_height, clock, run
-
-
-def draw_game_buttons():
-    draw_button(RETURN_BUTTON["x"], RETURN_BUTTON["y"], RETURN_BUTTON["width"], RETURN_BUTTON["height"],
-                "Return to Main Menu", lambda: menu())
-    draw_button(EXIT_BUTTON["x"], EXIT_BUTTON["y"], EXIT_BUTTON["width"], EXIT_BUTTON["height"], "Exit",
-                lambda: sys.exit())
 
 
 def run_game(game_title, hex_size, map_rows, map_cols, colored_percentage, difficulty=None):
@@ -219,7 +194,6 @@ def run_game(game_title, hex_size, map_rows, map_cols, colored_percentage, diffi
 
                     if valid_move:
                         player_turn = 3 - player_turn
-
 
                 if valid_move:
                     for hexagon in hexagons:
